702888d89453e9d9007dd507f241f2f0c3ecaaa1
AlexandreRoussel <alexandreroussel1201@gmail.com>
2017-10-18 17:37:29 +0200

    fucking api of shit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
-// $api_key = 'keyHB9YheEhsUXTkY';
-// $base = 'tblYvctTXVEt4hfzF/viw26e0XZwfKLIl6n';
-// $table = 'musics';
-// $airtable_url = 'https://api.airtable.com/v0/' . $base . '/' . $table;
-// $url = 'https://api.airtable.com/v0/' . $base . '/' . $table . '?maxRecords=10&view=Main%20View';
-// $headers = array(
-//     'Authorization: Bearer ' . $api_key
-// );
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_HTTPGET, 1);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// curl_setopt($ch, CURLOPT_URL, $url);
-// $entries = curl_exec($ch);
-// curl_close($ch);
-// $airtable_response = json_decode($entries, TRUE);
-
- var jsonSource = "https://api.airtable.com/v0/appTcNfokoNiW3RHR/musics?api_key=keyHB9YheEhsUXTkY";
-console.log(jsonSource);
-
+$(document).ready(function()
+    {
+        $.ajax({
+            url: "https://api.airtable.com/v0/appTcNfokoNiW3RHR/musics?api_key=keyHB9YheEhsUXTkY",
+            type:"GET",
+            data: "json",
+            jsonp: "jsoncallback",
+            success: function(data)
+            {console.log(data);
+                $("#template").tmpl(data.records).appendTo("#placeholder");
+            },
+            error: function(XMLHttpRequest, textStatus, ErrorThrown){console.log(XMLHttpRequest, textStatus, ErrorThrown)}
+        });
+    });
